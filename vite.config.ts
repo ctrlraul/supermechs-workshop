fd8fa4db0179eaf7010dc34dc13a4cb7c8a4b66b
ctrl-raul <mail.raulvitor@gmail.com>
2022-03-11 08:01:46 -0300

    Boiler plate

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import legacy from '@vitejs/plugin-legacy'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ]
+})
