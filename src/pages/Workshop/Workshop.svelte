2e30ea18fe413857cd2be8927ffdec8b4ce07ccb
ctrl-raul <mail.raulvitor@gmail.com>
2022-03-23 22:12:38 -0300

    Removed unused class from button

--- src/pages/Workshop/Workshop.svelte
+++ src/pages/Workshop/Workshop.svelte
@@ -241,7 +241,7 @@ function openPatreon (): void {
       <SvgIcon name="cog" color="var(--color-text)" />
     </button>
 
-    <button class="global-box patreon-button" on:click={openPatreon} use:tooltip={'Help me continue working on SuperMechs Workshop!'}>
+    <button class="global-box" on:click={openPatreon} use:tooltip={'Help me continue working on SuperMechs Workshop!'}>
       <SvgIcon name="patreon_logo" color="#FF424D" />
     </button>
 
