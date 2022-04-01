7bf92ecd9f17106f97fcacbacbde1e5709c53913
ctrl-raul <mail.raulvitor@gmail.com>
2022-04-01 07:58:50 -0300

    Cleaned code

--- src/battle/BattleEffectsHandler.ts
+++ src/battle/BattleEffectsHandler.ts
@@ -1,5 +1,6 @@
-import { cloneDeep } from 'lodash'
 import * as BattleAnimations from './BattleAnimations'
+import { cloneDeep } from 'lodash'
+import { getPlayerGfx } from '../BattleRenderer'
 
 
 
@@ -7,7 +8,6 @@ import * as BattleAnimations from './BattleAnimations'
 
 import type { Battle } from './Battle'
 import type { BattlePlayer } from './BattlePlayer'
-import { getPlayerGfx } from '../BattleRenderer'
 
 
 
@@ -18,26 +18,26 @@ export function fireDrone (battle: Battle, attacker: BattlePlayer, damage: numbe
   const { drone } = attacker
 
   if (drone === null) {
-	  throw new Error(`Failed to fire drone: ${attacker.name} does not have a drone equipped`)
+    throw new Error(`Failed to fire drone: ${attacker.name} does not have a drone equipped`)
   }
 
-	const oldState = cloneDeep(battle)
+  const oldState = cloneDeep(battle)
 
-	battle.dealDamagesAndTakeBackfire(attacker, drone, damage)
-	battle.updatePositions(attacker, drone)
-	battle.countItemUsage(attacker, drone)
+  battle.dealDamagesAndTakeBackfire(attacker, drone, damage)
+  battle.updatePositions(attacker, drone)
+  battle.countItemUsage(attacker, drone)
 
-	if (drone.stats.uses && drone.timesUsed === drone.stats.uses) {
-		// Uses are refilled when the drone is
-		// toggled on, so no need to do it here
-		attacker.droneActive = false
-	}
+  if (drone.stats.uses && drone.timesUsed === drone.stats.uses) {
+    // Uses are refilled when the drone is
+    // toggled on, so no need to do it here
+    attacker.droneActive = false
+  }
 
-	battle.pushLog(`*${attacker.name}*'s *${drone.name}* fired! (${damage} damage)`)
+  battle.pushLog(`*${attacker.name}*'s *${drone.name}* fired! (${damage} damage)`)
 
-	const newState = cloneDeep(battle)
+  const newState = cloneDeep(battle)
 
-	BattleAnimations.useWeapon(oldState, newState, attacker, drone, damage)
+  BattleAnimations.useWeapon(oldState, newState, attacker, drone, damage)
 
 }
 
@@ -48,28 +48,28 @@ export function fireDrone (battle: Battle, attacker: BattlePlayer, damage: numbe
  */
 export function forceCooldown (battle: Battle, player: BattlePlayer): boolean {
 
-	const double = player.stats.heat - player.stats.heaCol! > player.stats.heaCap;
-	const amount = player.stats.heaCol! * (double ? 2 : 1)
+  const double = player.stats.heat - player.stats.heaCol! > player.stats.heaCap;
+  const amount = player.stats.heaCol! * (double ? 2 : 1)
 
-	player.stats.heat = Math.max(0, player.stats.heat - amount)
+  player.stats.heat = Math.max(0, player.stats.heat - amount)
 
-	const newState = cloneDeep(battle)
+  const newState = cloneDeep(battle)
 
-	battle.pushLog(`*${player.name}* was forced to *${double ? 'double' : ''} cooldown* (-${amount} heat)`)
+  battle.pushLog(`*${player.name}* was forced to *${double ? 'double' : ''} cooldown* (-${amount} heat)`)
 
-	BattleAnimations.cooldown(newState, player, amount)
+  BattleAnimations.cooldown(newState, player, amount)
 
-	return double
-	
+  return double
+  
 }
 
 
 export function regenEnergy (player: BattlePlayer): void {
 
-	player.stats.energy = Math.min(
-		player.stats.eneCap,
-		player.stats.energy + player.stats.eneReg
-	)
+  player.stats.energy = Math.min(
+    player.stats.eneCap,
+    player.stats.energy + player.stats.eneReg
+  )
 
   getPlayerGfx(player.id).updateStats()
 
